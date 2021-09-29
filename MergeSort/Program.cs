ca595479f006972eaf398e23788a9fb91908ebda
Akash Randhawa <akashrandhawa@hotmail.com>
2021-09-29 16:36:42 -0700

    Merge sort initial test and implementation

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -7,16 +7,27 @@ namespace MergeSort
         static void Main(string[] args)
         {
 
-            ARRAY_SIZE = 1000;
+            int ARRAY_SIZE = 10;
             int[] arraySingleThread = new int[ARRAY_SIZE];
 
-
+            
 
 
             // TODO : Use the "Random" class in a for loop to initialize an array
+            Random rand = new Random();
+            int i;
+            for (i=0;  i<ARRAY_SIZE; i++)
+            {
+                arraySingleThread[i] = rand.Next(500);
+                //Console.WriteLine("index {0} = value {1}",i, arraySingleThread[i]);
+            }
 
             // copy array by value.. You can also use array.copy()
-            int[] arrayMultiThread = arraySingleThread.Slice(0,arraySingleThread.Length);
+            // int[] arrayMultiThread = arraySingleThread.Slice(0,arraySingleThread.Length);
+            int[] arrayMultiTread = new int[ARRAY_SIZE];
+            Array.Copy(arraySingleThread, arrayMultiTread, ARRAY_SIZE);
+            PrintArray(arraySingleThread);
+            PrintArray(arrayMultiTread);
 
             /*TODO : Use the  "Stopwatch" class to measure the duration of time that
                it takes to sort an array using one-thread merge sort and
@@ -25,7 +36,10 @@ namespace MergeSort
 
 
             //TODO :start the stopwatch
-            MergeSort(arraySingleThread);
+            int[] test = { 4, 4, 5, 8, 2, 2, 5, 4, 9, 7 };
+            
+            int [] sortedArray = MergeSort(arraySingleThread);
+            PrintArray(sortedArray);
             //TODO :Stop the stopwatch
 
 
@@ -38,31 +52,86 @@ namespace MergeSort
 
 
 
-             /*********************** Methods **********************
-              *****************************************************/
-             /*
-             implement Merge method. This method takes two sorted array and
-             and constructs a sorted array in the size of combined arrays
-             */
+            /*********************** Methods **********************
+             *****************************************************/
+            /*
+            implement Merge method. This method takes two sorted array and
+            and constructs a sorted array in the size of combined arrays
+            */
 
             static int[] Merge(int[] LA, int[] RA, int[] A)
             {
-
                 // TODO :implement
+                int k = 0;
+                int i = 0;
+                int j = 0;
 
+               
+                while ( i < LA.Length && j < RA.Length)
+                {
+                        
+                    if (LA[i] <= RA[j])
+                    {
+                        A[k] = LA[i];
+                        k++;
+                        i++;
+                    }
+                    else
+                    {
+                        A[k] = RA[j];
+                        j++;
+                        k++;
+                    }
+                        
+                }
+                while (i < LA.Length)
+                {
+                    A[k] = LA[i];
+                    i++;
+                    k++;
+                }
+                while (j < RA.Length)
+                {
+                    A[k] = RA[j];
+                    j++;
+                    k++;
+                }
+                
+                PrintArray(A);
+                return A;
             }
 
 
-             /*
-             implement MergeSort method: takes an integer array by reference
-             and makes some recursive calls to intself and then sorts the array
-             */
+            /*
+            implement MergeSort method: takes an integer array by reference
+            and makes some recursive calls to intself and then sorts the array
+            */
             static int[] MergeSort(int[] A)
             {
+                if (A.Length <= 1)
+                {
+                    return A;
+                }
 
-              // TODO :implement
+                int midpoint = (A.Length)/2;
+                int[] left = new int[midpoint];
+                int[] right = new int[A.Length-midpoint];
+                
+                for(int i=0; i<midpoint; i++)
+                {
+                    left[i] = A[i];
+                }
+                for (int i = 0; i < A.Length-midpoint; i++)
+                {
+                    right[i] = A[i+midpoint];
+                }
+                PrintArray(left);
+                PrintArray(right);
 
-
+                left = MergeSort(left);
+                right = MergeSort(right);
+                return (Merge(left, right, A));
+              
             }
 
 
