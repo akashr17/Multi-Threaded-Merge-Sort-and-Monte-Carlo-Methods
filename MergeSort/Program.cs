0594bd1e1ffb9b8b42c18517ff19532a0701975d
Akash Randhawa <akashrandhawa@hotmail.com>
2021-10-10 23:05:40 -0700

    Part 2 working

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -9,20 +9,20 @@ namespace MergeSort
     {
 
         private static Queue<int[]> qArray = new Queue<int[]>();
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Size of Array?");
             int ARRAY_SIZE = Convert.ToInt32(Console.ReadLine());
-            
+
             int[] arraySingleThread = new int[ARRAY_SIZE];
 
-            
+
 
 
             // TODO : Use the "Random" class in a for loop to initialize an array
             Random rand = new Random();
             int i;
-            for (i=0;  i<ARRAY_SIZE; i++)
+            for (i = 0; i < ARRAY_SIZE; i++)
             {
                 arraySingleThread[i] = rand.Next(1000);
                 //Console.WriteLine("index {0} = value {1}",i, arraySingleThread[i]);
@@ -73,7 +73,7 @@ namespace MergeSort
             Console.WriteLine("is sorted {0}", IsSorted(sortedMultiThreadArray));
             Console.WriteLine("Time for multi tread is {0}", ts1);
 
-
+            Console.WriteLine("Speed up factor is {0}", ts.TotalSeconds / ts1.TotalSeconds);
 
 
 
@@ -83,161 +83,162 @@ namespace MergeSort
             implement Merge method. This method takes two sorted array and
             and constructs a sorted array in the size of combined arrays
             */
-
-            static int[] Merge(int[] LA, int[] RA, int[] A)
-            {
-                // TODO :implement
-                int k = 0;
-                int i = 0;
-                int j = 0;
+        }
+        static int[] Merge(int[] LA, int[] RA, int[] A)
+        {
+            // TODO :implement
+            int k = 0;
+            int i = 0;
+            int j = 0;
 
                
-                while ( i < LA.Length && j < RA.Length)
-                {
+            while ( i < LA.Length && j < RA.Length)
+            {
                         
-                    if (LA[i] <= RA[j])
-                    {
-                        A[k] = LA[i];
-                        k++;
-                        i++;
-                    }
-                    else
-                    {
-                        A[k] = RA[j];
-                        j++;
-                        k++;
-                    }
-                        
-                }
-                while (i < LA.Length)
+                if (LA[i] <= RA[j])
                 {
                     A[k] = LA[i];
-                    i++;
                     k++;
+                    i++;
                 }
-                while (j < RA.Length)
+                else
                 {
                     A[k] = RA[j];
                     j++;
                     k++;
                 }
+                        
+            }
+            while (i < LA.Length)
+            {
+                A[k] = LA[i];
+                i++;
+                k++;
+            }
+            while (j < RA.Length)
+            {
+                A[k] = RA[j];
+                j++;
+                k++;
+            }
                 
-                //PrintArray(A);
+            //PrintArray(A);
+            return A;
+        }
+
+
+        /*
+        implement MergeSort method: takes an integer array by reference
+        and makes some recursive calls to intself and then sorts the array
+        */
+        static int[] MergeSort(int[] A)
+        {
+            if (A.Length <= 1)
+            {
                 return A;
             }
 
-
-            /*
-            implement MergeSort method: takes an integer array by reference
-            and makes some recursive calls to intself and then sorts the array
-            */
-            static int[] MergeSort(int[] A)
-            {
-                if (A.Length <= 1)
-                {
-                    return A;
-                }
-
-                int midpoint = (A.Length)/2;
-                int[] left = new int[midpoint];
-                int[] right = new int[A.Length-midpoint];
+            int midpoint = (A.Length)/2;
+            int[] left = new int[midpoint];
+            int[] right = new int[A.Length-midpoint];
                 
-                for(int i=0; i<midpoint; i++)
-                {
-                    left[i] = A[i];
-                }
-                for (int i = 0; i < A.Length-midpoint; i++)
-                {
-                    right[i] = A[i+midpoint];
-                }
-                //PrintArray(left);
-                //PrintArray(right);
+            for(int i=0; i<midpoint; i++)
+            {
+                left[i] = A[i];
+            }
+            for (int i = 0; i < A.Length-midpoint; i++)
+            {
+                right[i] = A[i+midpoint];
+            }
+            //PrintArray(left);
+            //PrintArray(right);
 
-                left = MergeSort(left);
-                right = MergeSort(right);
-                return (Merge(left, right, A));
+            left = MergeSort(left);
+            right = MergeSort(right);
+            return (Merge(left, right, A));
               
-            }
+        }
             
 
-            static int[] MultiThreadSort(int [] A, int num)
-            {
-                // split recurrsively by taking away length of previous array
-                int subSize = 0;
-                int sum = 0;
-                // 2d array for storing all of the subs
-                int[][] sub = new int[num][];
+        static int[] MultiThreadSort(int [] A, int num)
+        {
+            // split recurrsively by taking away length of previous array
+            int subSize = 0;
+            int sum = 0;
+            // 2d array for storing all of the subs
+            int[][] sub = new int[num][];
 
-                Thread[] t = new Thread[num];
+            Thread[] t = new Thread[num];
                 
-                int temp = 0;
-                // populate subarrays
-                for (int i = 0; i < num; i++)
-                {
-                    temp = i;
-                    subSize = (A.Length-sum) / (num-temp);
+            int temp = 0;
+            // populate subarrays
+            for (int i = 0; i < num; i++)
+            {
+                   
+                subSize = (A.Length-sum) / (num-i);
                     
-                    sub[temp] = new int[subSize];
-                    Array.Copy(A, sum, sub[temp], 0, subSize);
-                    //PrintArray(sub[temp]);
-                    t[temp] = new Thread(() => qArray.Enqueue(MergeSort(sub[temp])));
-                    t[temp].Start();
-                    
-                    sum += subSize;
-                }
-
-                for (int i = 0; i < num; i++)
-                {
-                    t[i].Join();
-                }
-
-                while (qArray.Count > 1)
-                {
-                    int[] temp1 = qArray.Dequeue();
-                    int[] temp2 = qArray.Dequeue();
-                    int[] temp3 = new int[temp1.Length + temp2.Length];
-                    Merge(temp1, temp2, temp3);
-                    qArray.Enqueue(temp3);
-                }
-                return qArray.Dequeue();
+                sub[i] = new int[subSize];
+                Array.Copy(A, sum, sub[i], 0, subSize);
+                //PrintArray(sub[temp]);
+                   
+                sum += subSize;
             }
 
-            static void multi(int [] a)
+            for (int i=0; i<num; i++)
             {
-                //MergeSort(a);
-                //PrintArray(a);
-                qArray.Enqueue(MergeSort(a));
+                temp = i;
+                t[temp] = new Thread(() => qArray.Enqueue(MergeSort(sub[temp])));
+                t[temp].Start();
             }
 
-            // a helper function to print your array
-            static void PrintArray(int[] myArray)
+            for (int i = 0; i < num; i++)
             {
-                Console.Write("[");
-                for (int i = 0; i < myArray.Length; i++)
-                {
-                    Console.Write("{0} ", myArray[i]);
-
-                }
-                Console.Write("]");
-                Console.WriteLine();
-
+                t[i].Join();
             }
 
-            // a helper function to confirm your array is sorted
-            // returns boolean True if the array is sorted
-            static bool IsSorted(int[] a)
+            while (qArray.Count > 1)
             {
-                int j = a.Length - 1;
-                if (j < 1) return true;
-                int ai = a[0], i = 1;
-                while (i <= j && ai <= (ai = a[i])) i++;
-                return i > j;
+                int[] temp1 = qArray.Dequeue();
+                int[] temp2 = qArray.Dequeue();
+                int[] temp3 = new int[temp1.Length + temp2.Length];
+                Merge(temp1, temp2, temp3);
+                qArray.Enqueue(temp3);
             }
+            return qArray.Dequeue();
+        }
 
-            
+        static void multi(int [] a)
+        {
+            //MergeSort(a);
+            //PrintArray(a);
+            qArray.Enqueue(MergeSort(a));
+        }
 
+        // a helper function to print your array
+        static void PrintArray(int[] myArray)
+        {
+            Console.Write("[");
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write("{0} ", myArray[i]);
+
+            }
+            Console.Write("]");
+            Console.WriteLine();
 
         }
+
+        // a helper function to confirm your array is sorted
+        // returns boolean True if the array is sorted
+        static bool IsSorted(int[] a)
+        {
+            int j = a.Length - 1;
+            if (j < 1) return true;
+            int ai = a[0], i = 1;
+            while (i <= j && ai <= (ai = a[i])) i++;
+            return i > j;
+        }
+
 
 
     }
--- MultiThreadPi/MultiThreadPi/Program.cs
+++ MultiThreadPi/MultiThreadPi/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace MultiThreadPi
 {
@@ -6,20 +9,102 @@ namespace MultiThreadPi
     {
         static void Main(string[] args)
         {
-            long numberOfSamples = 1000;
-            long hits;
-            double pi = EstimatePI(numberOfSamples, ref long hits);
+            long numberOfSamples = 10000;
+            long hits = 0;
+            long singleThreadHits = 0;
 
-            static double EstimatePI(long numberOfSamples, ref long hits)
+
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            EstimatePI(numberOfSamples, ref singleThreadHits);
+
+            double singlePI = 4.0 * singleThreadHits / numberOfSamples;
+
+            stopWatch.Stop();
+
+            TimeSpan ts = stopWatch.Elapsed;
+
+
+
+
+
+           
+            Stopwatch stopWatch1 = new Stopwatch();
+            stopWatch1.Start();
+
+            int numOfThreads = 4;
+            int sum = 0;
+
+            Thread[] t = new Thread[numberOfSamples];
+
+            for (int i = 0; i < numOfThreads; i++)
             {
-                //implement
+                int temp = i;
+                int samplePerThread = ((int)numberOfSamples-sum)/(numOfThreads-temp);
+                t[temp] = new Thread(() => EstimatePI(samplePerThread, ref hits));
+                t[temp].Start();
+                sum+=samplePerThread;
+
+            }
+            for (int i = 0; i < numOfThreads; i++)
+            {
+                t[i].Join();
             }
 
-            static double[,] GenerateSamples(long numberOfSamples)
-            {
-                // Implement  
+            double pi = 4.0 * hits / numberOfSamples;
 
-            }
+            stopWatch1.Stop();
+
+            TimeSpan ts1 = stopWatch1.Elapsed;
+            
+           
+
+            Console.WriteLine("Single threaded pi = {0} and took {1} seconds", singlePI, ts);
+            Console.WriteLine("Multi threaded pi = {0} and took {1} seconds", pi, ts1);
+            Console.WriteLine("Speed up factor = {0}", ts.TotalSeconds/ts1.TotalSeconds);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+
         }
+        
+        static void EstimatePI(long numberOfSamples, ref long hits)
+        {
+            double[,] samples = GenerateSamples(numberOfSamples);
+            
+
+            for (int i = 0; i < numberOfSamples; i++)
+            {
+                double x = samples[i, 0];
+                double y = samples[i, 1];
+
+                if ((x * x) + (y * y) <= 1)
+                {
+                    Interlocked.Increment(ref hits);
+                }
+
+            }
+            
+
+            
+        }
+        static double[,] GenerateSamples(long numberOfSamples)
+        {
+            // Implement 
+            Random test = new Random();
+            double[,] samples = new double[numberOfSamples,2];
+
+            for (int i = 0; i < numberOfSamples; i++)
+            {
+                samples[i,0] = test.NextDouble() * 2 - 1.0;
+                samples[i,1] = test.NextDouble() * 2 - 1.0;
+                
+            }
+            
+            return samples;
+        }
+        
+        
     }
 }
